1a9e6d9a51055f23e1185b984c3b9e7e25681f6c
Deepak Tiwari <ainutiwar483@gmail.com>
2022-07-20 00:13:01 +0530

    add first two files

---
+++ VariableNames.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int value = 15;
+    // string name = "Deepak Tiwari";
+    // double fractiondigit = -15.45;
+    // char singlechar = 'B';
+    // bool isTrue = false;
+    // bool isFalse = true;
+    // cout << isTrue << endl;
+    // cout << isFalse << endl;
+    // cout << singlechar << endl;
+    // cout << fractiondigit << endl;
+    // cout << name << endl;
+    // cout << value << endl;
+    // if(isTrue == false){
+    //     cout << "isTrue is false" << endl;
+    // }
+    // else{
+    //     cout << "isTrue is true" << endl;
+    // }
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // this is comment and it is not compiled
+    cout << "Hello world!\n";
+    cout << "I'm learning c++";
+    return 0;
+}
