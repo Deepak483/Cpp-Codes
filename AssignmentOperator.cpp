4141026025ef74913cb141c6d14b17c0051369f0
Deepak Tiwari <ainutiwar483@gmail.com>
2022-07-21 19:13:28 +0530

    add new file and made changes in assignment file

--- AssignmentOperator.cpp
+++ AssignmentOperator.cpp
@@ -6,17 +6,20 @@ int main()
     string greeting = "Hello, world!";
     cout << greeting << endl;
     // greeting.size();
-    greeting.append("!");// some string methods
+    greeting.append("!"); // some string methods
     cout << greeting << endl;
     cout << greeting.size() << endl;
     string firstName = "Deepak";
     string lastName = "Tiwari";
     string fullName = firstName + " " + lastName;
-    //same line with append method
-    cout<<firstName.append(lastName);
+    // same line with append method
+    cout << firstName.append(lastName) << endl;
     // cout << fullName << endl;
-    //trying to add string with number
+    // trying to add string with number
     string x = "10";
     int y = 20;
     // cout << x + y << endl; this line will give error because of type mismatch  between string and int
+    // string length function
+    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << "The length of the txt string is: " << txt.length();
 }
