7527b04eb27c10f211af1f30a58785a3bb1dff03
Deepak Tiwari <ainutiwar483@gmail.com>
2022-08-03 13:35:15 +0530

    function example file modified

--- FunctionExample.cpp
+++ FunctionExample.cpp
@@ -14,33 +14,48 @@
 // }
 using namespace std;
 int maximum(int, int);
-void printMessage()
-{
-    cout << "Hello World!!" << endl;
-}
-int maximum(int a, int b)
-{
-    if (a > b)
-    {
-        return a;
-    }
-    else
-    {
-        return b;
-    }
-}
+// void printMessage()
+// {
+//     cout << "Hello World!!" << endl;
+// }
+// int maximum(int a, int b)
+// {
+//     if (a > b)
+//     {
+//         return a;
+//     }
+//     else
+//     {
+//         return b;
+//     }
+// }
 void myFunction(string fname)
 {
     cout << "Hello " << fname << endl;
 }
+void myFunction1(string country = "India") // default parameter
+{
+    cout << country << "\n";
+}
+int myFunction2(int, int);
 int main()
 {
-    int a, b;
-    cout << "Enter the two numbers: " << endl;
-    cin >> a >> b;
+    // int a, b;
+    // cout << "Enter the two numbers: " << endl;
+    // cin >> a >> b;
 
-    int c = maximum(a, b);
-    cout << "The maximum number is: " << c << endl;
-    printMessage();
+    // int c = maximum(a, b);
+    // cout << "The maximum number is: " << c << endl;
+    // printMessage();
     myFunction("Raj");
+    myFunction1();
+    myFunction1("USA");
+    myFunction1("Sweden");
+    int z = myFunction2(5, 3);
+    cout << z << endl;
+    return 0;
+}
+int myFunction2(int x, int y)
+{
+    return x + y;
 }
