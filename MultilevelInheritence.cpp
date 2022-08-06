4fea58de2b3298a5d73080a9bea43f48c7b99a24
Deepak Tiwari <ainutiwar483@gmail.com>
2022-08-06 21:07:42 +0530

    Multilevel inheritance file is added

---
+++ MultilevelInheritence.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+// parent class
+class Person
+{
+public:
+    string name;
+    int age;
+
+    void introduce()
+    {
+        cout << "My name is " << name << " and I am " << age << " years old." << endl;
+    }
+};
+
+// child class
+class Child : public Person
+{
+public:
+    string hair_color;
+    void action()
+    {
+        cout << "I can do things like walk, talk and speak." << endl;
+    }
+};
+
+// grandchild class
+class GrandChild : public Child
+{
+public:
+    void eat()
+    {
+        cout << "I can eat." << endl;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
