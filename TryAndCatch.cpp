39d0a94d6208aad1df302ac5dd99a7a663a294cd
ainutiwari483@gmail.com <ainutiwari483@gmail.com>
2022-08-08 14:47:50 +0530

    try and catch file is modified and added successfully

--- TryAndCatch.cpp
+++ TryAndCatch.cpp
@@ -2,4 +2,25 @@
 using namespace std;
 int main()
 {
+    try
+    {
+        int age = 15;
+        if (age >= 18)
+        {
+            cout << "Access granted--you are old enough to vote" << endl;
+        }
+        else
+        {
+            // throw age;
+            throw 505;
+        }
+    }
+    // catch (int myAge)
+    catch (...)
+    {
+        cout << "Access denied - You must be at least 18 years old to vote" << endl;
+        // cout << "Age is: " << myAge << endl;
+        // cout << "Error no: " << myAge << endl;
+        // cout << "Error no:" << endl;
+    }
 }
