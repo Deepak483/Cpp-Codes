b6bbd12559bef8fbf4227e885c506d6ba7020826
Deepak Tiwari <ainutiwar483@gmail.com>
2022-08-13 23:24:41 +0530

    enum and example file modified

--- EnumWithSwitch.cpp
+++ EnumWithSwitch.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     enum class Season
-    {
+    {   
         spring,
         summer,
         fall,
---
+++ example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int scores[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int count{size(scores)};
+    for (size_t i{0}; i < count; i++)
+    {
+        cout << "scores[" << i << "] : " << scores[i] << endl;
+    }
+
+    return 0;
+}
