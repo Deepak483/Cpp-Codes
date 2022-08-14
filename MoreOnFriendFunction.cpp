a81372b704439ca29a9028bfd7855eb2eb45cb5e
Deepak Tiwari <ainutiwar483@gmail.com>
2022-08-14 16:39:54 +0530

    modified moreonfriend and example file

--- MoreOnFriendFunction.cpp
+++ MoreOnFriendFunction.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+// forward declaration.
+class Y;
 class X
 {
     int data1; // variable data
@@ -8,6 +10,7 @@ public:
     {
         data1 = value;
     }
+    friend void add(X, Y);
 };
 class Y
 {
@@ -18,13 +21,18 @@ public:
     {
         data2 = value;
     }
+    friend void add(X, Y);
 };
-
+void add(X o1, Y o2)
+{
+    cout << "Addition of data of X and Y gives " << o1.data1 + o2.data2 << endl;
+}
 int main()
 {
     X x;
     Y y;
     x.setValue(10);
     y.setValue(20);
+    add(x, y);
     return 0;
 }
--- example.cpp
+++ example.cpp
@@ -4,10 +4,13 @@ int main()
 {
     int scores[] = {1, 2, 3, 4, 5, 6, 7, 8};
     int count{size(scores)};
-    for (size_t i{0}; i < count; i++)
-    {
-        cout << "scores[" << i << "] : " << scores[i] << endl;
-    }
+    cout << "sizeof(scores): " << sizeof(scores) << endl;
+    cout << "sizeof(scores[0]): " << sizeof(scores[0]) << endl;
+    // for (size_t i{0}; i < count; i++)
+    // {
+    //     cout << "scores[" << i << "] : " << scores[i] << endl;
+    // }
+    
 
     return 0;
 }
