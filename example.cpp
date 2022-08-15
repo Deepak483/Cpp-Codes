a3971da5d98bf942c4a6c026dd2fadcb0bcdc2ac
ainutiwari483@gmail.com <ainutiwari483@gmail.com>
2022-08-15 13:56:10 +0530

    some files added like example and scope

--- example.cpp
+++ example.cpp
@@ -1,19 +1,40 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int scores[] = {1, 3, 3, 899, 382, 88};
+//     // int count{std::size(scores)};
+//     cout << "sizeof(scores) :" << sizeof(scores) << endl;
+//     cout << "sizeof(scores[0]) :" << sizeof(scores[0]) << endl;
+//     int count{sizeof(scores) / sizeof(scores[0])};
+
+//     // for (size_t i{}; i < count; i++)
+//     // {
+//     //     cout << "scores[" << i << "] :" << scores[i] << endl;
+//     // }
+//     for (auto i : scores)
+//     {
+//         cout << "value: " << i << std::endl;
+//     }
+//     char name[] = {'H', 'e', 'l', 'l', 'o'};
+//     for (char i : name)
+//     {
+//         cout << "name: " << i << endl;
+//     }
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-#define X 10 // defining constant value using macros
 int main()
 {
-    // const int x = 5;
-    // enum
-    // {
-    //     y = 6,
-    //     z = 7
-    // }; // defining value using enumeration which is constant
-    // cout << y << endl;
-    // cout << z << endl;
-    auto x = "this is string";
-    cout<<x<<endl;
+    // Declare array
+    char messages[5] = {'H', 'e', 'l', 'l', 'o'};
+    cout << "messages :" << endl;
+    for (auto c : messages)
+    {
+        cout << c;
+    }
 
-    
     return 0;
 }
