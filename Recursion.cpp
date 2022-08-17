d084f290b57d013d3954207efafe85cdf1dadc14
Deepak Tiwari <ainutiwar483@gmail.com>
2022-08-17 23:22:09 +0530

    added few line in Recursion file

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,21 +1,49 @@
-// sample program of recursion
+// // sample program of recursion
+// #include <iostream>
+// using namespace std;
+
+// int addfun(int n)
+// {
+//     if (n > 0)
+//     {
+//         return n + addfun(n - 1);
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
+// int main()
+// {
+//     int sum = addfun(10);
+//     cout << sum << endl;
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-
-int addfun(int n)
+int factorial(int);
+int main()
 {
-    if (n > 0)
+    int fact, value;
+    cout << "Enter the value:" << endl;
+    cin >> value;
+    fact = factorial(value);
+    cout << "Factorial of the " << value << " : " << fact << endl;
+    return 0;
+}
+int factorial(int n)
+{
+    if (n < 0)
     {
-        return n + addfun(n - 1);
+        return -1;
+    }
+    else if (n == 0)
+    {
+        return 1;
     }
     else
     {
-        return 0;
+        return n * factorial(n - 1);
     }
-}
-int main()
-{
-    int sum = addfun(10);
-    cout << sum << endl;
-    return 0;
 }
