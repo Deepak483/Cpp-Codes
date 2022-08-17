36c27c857ee1db258c88124fe7ade50e677ef27f
ainutiwari483@gmail.com <ainutiwari483@gmail.com>
2022-08-17 14:25:38 +0530

    UnionExample file is modified again

--- UnionExample.cpp
+++ UnionExample.cpp
@@ -19,20 +19,32 @@ union book
     float bookPrice;
 };
 
+union money
+{
+    int rupees;
+    float dollar;
+};
+
 int main()
 {
-    union book bk;
-    cout << "Enter Book Id No: " << endl;
-    cin >> bk.bookId;
-    cout << "Enter Book Name: " << endl;
-    // cin >> bk.bookName;
-    cout << "Enter price of book: " << endl;
-    cin >> bk.bookPrice;
+    // union book bk;
+    // cout << "Enter Book Id No: " << endl;
+    // cin >> bk.bookId;
+    // cout << "Enter Book Name: " << endl;
+    // // cin >> bk.bookName;
+    // cout << "Enter price of book: " << endl;
+    // cin >> bk.bookPrice;
 
-    cout << endl;
-    cout << "Book Id No: " << bk.bookId << endl;
-    cout << "Book Name : " << bk.bookId << endl;
-    cout << "Book price : " << bk.bookId << endl;
+    // cout << endl;
+    // cout << "Book Id No: " << bk.bookId << endl;
+    // cout << "Book Name : " << bk.bookId << endl;
+    // cout << "Book price : " << bk.bookId << endl;
+    union money my;
+    my.rupees = 100;
+    my.dollar = 400;
+    cout << my.rupees << endl;
+    my.dollar = 200;
+    cout << my.dollar << endl;
 
     return 0;
 }
